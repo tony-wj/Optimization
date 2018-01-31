49647c6f8700dce465ad3828f67f410206a2199b
Tony Wu <wujian.tony@gmail.com>
2018-01-31 17:36:02 -0600

    0-1 knapsack using GA

--- genetic.cpp
+++ genetic.cpp
@@ -1,9 +1,10 @@
+/*A cpp program that solves 0-1 knapsack problem by genetic algorithm*/
 #include <iostream>
 #include <ctime>
 using namespace std;
 #define OBJ_COUNT 7
 #define CAPACITY 150
-#define POPULATION_SIZE 32
+#define POPULATION 32
 #define P_XOVER 0.8
 #define P_MUTATION 0.15
 
@@ -20,19 +21,19 @@ typedef struct GAType
 int EnvaluateFitness(GATYPE *pop) 
 { 
 	int totalFitness = 0; 
-	for( int i = 0; i < POPULATION_SIZE; i++) 
+	for( int i = 0; i < POPULATION; i++) 
 	{ 
 		int tw = 0; 
-		pop[ i]. fitness = 0; 
+		pop[i]. fitness = 0; 
 		for( int j = 0; j < OBJ_COUNT; j++) 
 		{ 
-			if( pop[i].gene[ j] == 1) 
+			if( pop[i].gene[j] == 1) 
 			{ 
-				tw += Weight[ j]; 
-				pop[i].fitness += Value[ j]; 
+				tw += Weight[j]; 
+				pop[i].fitness += Value[j]; 
 			} 
 		}
-		if( tw > CAPACITY) /*penalty*/ 
+		if( tw > CAPACITY) /*Penalty*/ 
 		{ 
 			pop[i].fitness = 1; 
 		} 
@@ -41,7 +42,7 @@ int EnvaluateFitness(GATYPE *pop)
 	return totalFitness; 
 }
 
-void ExchangeOver( GATYPE *pop, int first, int second)
+void ExchangeOver(GATYPE *pop, int first, int second)
 { 
 /*Exchange random number of genes*/ 
 	int ecc = rand() % OBJ_COUNT + 1; 
@@ -49,86 +50,86 @@ void ExchangeOver( GATYPE *pop, int first, int second)
 	{ 
 	/*The probability of exchange is the same*/ 
 		int idx = rand() % OBJ_COUNT; 
-		int tg = pop[ first]. gene[ idx]; 
-		pop[ first]. gene[ idx] = pop[ second]. gene[ idx]; 
-		pop[ second]. gene[ idx] = tg; 
+		int tg = pop[first]. gene[idx]; 
+		pop[first]. gene[idx] = pop[second]. gene[idx]; 
+		pop[second]. gene[idx] = tg; 
 	} 
 }
 
-void Crossover( GATYPE *pop) 
+void Crossover(GATYPE *pop) 
 { 
 	int first = -1;
 	// A signal that the first individual is selected
-	for( int i = 0; i < POPULATION_SIZE; i++) 
+	for( int i = 0; i < POPULATION; i++) 
 	{ 
 		double p = (double) rand() / RAND_MAX; 
-		if( p < P_XOVER) 
+		if(p < P_XOVER) 
 		{
-			if( first < 0) 
+			if(first < 0) 
 			{ 
 			first = i; //Select the first individual
 			} else 
 			{ 
-			ExchangeOver( pop, first, i); 
+			ExchangeOver(pop, first, i); 
 			first = -1;// Clear the signal
 			} 
 		} 
 	} 
 }
 
-void ReverseGene( GATYPE *pop, int index) 
+void ReverseGene(GATYPE *pop, int index) 
 { 
 /*Reverse random number of genes*/ 
 	int mcc = rand() % OBJ_COUNT + 1; 
-	for( int i = 0; i < mcc; i++) 
+	for(int i = 0; i < mcc; i++) 
 	{ 
 	/*The probability of reverse is the same*/ 
 		int gi = rand() % OBJ_COUNT; 
-		pop[ index]. gene[ gi] = 1 - pop[ index]. gene[ gi];
+		pop[index].gene[gi] = 1 - pop[index].gene[gi];
 	}
 }
 
-void Mutation( GATYPE *pop) 
+void Mutation(GATYPE *pop) 
 { 
-	for( int i = 0; i < POPULATION_SIZE; i++) 
+	for(int i = 0; i < POPULATION; i++) 
 	{ 
 		double p = (double) rand() / RAND_MAX;
-		if( p < P_MUTATION) 
+		if(p < P_MUTATION) 
 		{ 
-			ReverseGene( pop, i); 
+			ReverseGene(pop, i); 
 		} 
 	} 
 }
 
 void Select(int totalFitness, GATYPE *pop)
 {
-	GATYPE newPop[POPULATION_SIZE] = { 0 };
+	GATYPE newPop[POPULATION] = { 0 };
 	double lastCf = 0.0; 
 	//calculate the individual's selection frequency and cumulative frequency 
-	for( int i = 0; i < POPULATION_SIZE; i++) 
+	for(int i = 0; i < POPULATION; i++) 
 	{ 
 		pop[i].rf = (double) pop[i].fitness / totalFitness; 
 		pop[i].cf = lastCf + pop[i].rf; 
 		lastCf = pop[i].cf; 
 	}
-	for( int i = 0; i < POPULATION_SIZE; i++) 
+	for(int i = 0; i < POPULATION; i++) 
 	{ 
 		double p = (double)rand() / RAND_MAX; 
-		if( p < pop[0]. cf)
+		if( p < pop[0].cf)
 		{ 
 			newPop[i] = pop[ 0]; 
 		} else 
 		{ 
-			for( int j = 0; j < POPULATION_SIZE; j++) 
+			for( int j = 0; j < POPULATION; j++) 
 			{ 
-				if(( p >= pop[j]. cf) && (p < pop[j + 1]. cf)) 
+				if(( p >= pop[j].cf) && (p < pop[j + 1].cf)) 
 				{ 
 					newPop[i] = pop[j + 1]; 
 				} 
 			} 
 		} 
 	}
-	for (int i = 0; i < POPULATION_SIZE; ++i)
+	for (int i = 0; i < POPULATION; ++i)
 	{
 		pop[i]=newPop[i];
 	}
@@ -137,7 +138,7 @@ void Select(int totalFitness, GATYPE *pop)
 void Initialize(GATYPE *pop)
 {
 	//generate some random numbers
-	for (int i = 0; i < POPULATION_SIZE; ++i)
+	for (int i = 0; i < POPULATION; ++i)
 	{
 		for (int j = 0; j < OBJ_COUNT; ++j)
 		{
@@ -151,7 +152,7 @@ void Print(GATYPE *pop)
 {
 	int maxfit = 0;
 	int maxaug = 0;
-	for (int i = 0; i < POPULATION_SIZE; ++i)
+	for (int i = 0; i < POPULATION; ++i)
 	{
 		if (pop[i].fitness > maxfit)
 		{
@@ -167,17 +168,23 @@ void Print(GATYPE *pop)
 
 int main(int argc, char **argv){
 	srand((unsigned)time(0));
-	GATYPE population[POPULATION_SIZE] = { 0 }; 
-	Initialize(population); 
-	int totalFitness = EnvaluateFitness( population); 
+	//get the input as max generations
 	string str = argv[1];
-	int max_generations = stoi(str);
-	for( int i = 0; i < max_generations; i++) 
+	int MAX_GENERATIONS = stoi(str);
+	//create a GATYPE popultaion
+	GATYPE population[POPULATION] = { 0 }; 
+	//initialization
+	Initialize(population); 
+	//calculate the total fitness
+	int totalFitness = EnvaluateFitness(population); 
+	//loop for MAX_GENERATIONS
+	for( int i = 0; i < MAX_GENERATIONS; i++) 
 	{ 
-		Select( totalFitness, population); 
-		Crossover( population); 
-		Mutation( population); 
-		totalFitness = EnvaluateFitness( population); 
+		Select(totalFitness, population); 
+		Crossover(population); 
+		Mutation(population); 
+		totalFitness = EnvaluateFitness(population); 
 	}
+	//Print out the result
 	Print(population);
 }
